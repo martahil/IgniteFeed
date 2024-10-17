639f1c8cc0157f87490ff0f9e9831d5065115520
Marta Hilgert <marta.hil.1988@gmail.com>
2024-10-17 17:46:10 -0300

    Implement comment deletion feature

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/74906513?v=4" alt="" />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
               <time title='May 11 at 08:13' dateTime='2022-05-11 08:13:30'>About 1 hour ago</time>
             </div>
 
-            <button title='Delete comment'>
+            <button onClick={handleDeleteComment} title='Delete comment'>
               <Trash size={24} />
             </button>
           </header>
