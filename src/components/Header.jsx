39d18184a1c56c26b4a118f73a94ee2d67be81cf
Marta Hilgert <marta.hil.1988@gmail.com>
2024-10-16 21:57:48 -0300

    Creating keys (Post content & Comments)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,6 @@ import styles from './Header.module.css'
 
 import igniteLogo from '../assets/ignite-logo.svg'
 
-console.log(igniteLogo)
-
-console.log(styles)
-
 export function Header() {
   return (
     <header className={styles.header}>
